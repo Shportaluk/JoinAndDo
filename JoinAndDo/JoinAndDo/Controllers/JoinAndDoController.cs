f94414663e69963daaae284742de40b3396a87b8
Shportaluk <andreyshportaluk@gmail.com>
2015-12-04 00:42:42 +0200

    04-12-2015 0:42
    Add Project ( Join And Do )

---
+++ JoinAndDo/JoinAndDo/Controllers/JoinAndDoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoinAndDo.Controllers
+{
+    public class JoinAndDoController : Controller
+    {
+        // GET: JoinAndDo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult my_accession()
+        {
+            return View();
+        }
+
+        public ActionResult my_message()
+        {
+            return View();
+        }
+
+        public ActionResult my_profile()
+        {
+            return View();
+        }
+
+        public ActionResult deals_accession()
+        {
+            return View();
+        }
+
+        public ActionResult test()
+        {
+            return View();
+        }
+    }
+}
---
+++ JoinAndDo/JoinAndDo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("JoinAndDo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JoinAndDo")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("946c75f9-2386-4bcd-9163-48edd5c5c291")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      дополнительный номер версии;
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
