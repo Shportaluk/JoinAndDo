ec7035b11fca533a0f2ab8c4ab254a5e996d01d8
Shportaluk <andreyshportaluk@gmail.com>
2015-12-12 20:21:41 +0200

    12-12-2015 20:21
    Add class JoinsEntity

---
+++ JoinAndDo/JoinAndDo.Entities/JoinsEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoinAndDo.Entities
+{
+    public class JoinsEntity
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public JoinsEntity(  )
+        {
+            this.Title = "";
+            this.Text = "";
+        }
+
+        public JoinsEntity( string title, string text )
+        {
+            this.Title = title;
+            this.Text = text;
+        }
+    }
+}
